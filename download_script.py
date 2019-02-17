0283d8d8a47e4e70f9319a2d5ef2f557c425d473
Kristian Behrendt <kristianbg20@mail.com>
2019-02-17 19:56:25 +0100

    clearn up and part 3

--- download_script.py
+++ download_script.py
@@ -3,3 +3,4 @@ import sys
 
 for url in sys.argv[1:]:
     webget.download_2(url)
+    
--- mymodule.py
+++
@@ -1,9 +0,0 @@
-"""This is a really cool module!"""
-
-class SomeClass():
-
-    def __init__(self, stuff):
-        print('I can print ', stuff)
-
-def some_fun(stuff):
-    return 'I am a function that returns ' + str(stuff)
